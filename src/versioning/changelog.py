e57e2892af425e498387a11d4f67c0050fa8a078
Jesse P. Johnson <jpj6652@gmail.com>
2022-03-08 21:35:53 -0500

    feat: added changelog

--- src/versioning/changelog.py
+++ src/versioning/changelog.py
@@ -3,17 +3,66 @@
 import logging
 import re
 from datetime import datetime
-from typing import TYPE_CHECKING, Dict, List
+from typing import TYPE_CHECKING, Any, Dict, List
 
 from mdutils.mdutils import MdUtils
 from versioning.grammars.conventional_commits import CommitMessageParser
 
 if TYPE_CHECKING:
-    from pygit2 import Repository
+    from pygit2 import Commit, Repository
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 SCOPES = ['added', 'changed', 'deprecated', 'removed', 'fixed', 'security']
+
+
+def get_release_changes(
+    current_commit: 'Commit',
+    repo: 'Repository'
+) -> Dict[str, Any]:
+    """Get changes associated with a release."""
+    parser = CommitMessageParser()
+    changes: Dict[str, List[str]] = {
+        'added': [],
+        'changed': [],
+        'deprecated': [],
+        'removed': [],
+        'fixed': [],
+        'security': [],
+    }
+
+    for commit in repo.walk(repo.head.target):
+        # check if commit is tagged and stop if it is
+        parser.parse(commit.message.rstrip())
+        scope = parser.title['scope']
+        row = [
+            commit.hex,
+            parser.title['type'],
+            parser.title['description']
+        ]
+
+        if scope is not None:
+            if scope not in changes:
+                changes[scope] = []
+            changes[scope].extend(row)
+        elif parser.title['type'] == 'feat':
+            changes['added'].extend(row)
+        elif parser.title['type'] == 'fix':
+            changes['fixed'].extend(row)
+        elif (
+            parser.title['type'] == 'refactor'
+            or parser.title['type'] == 'ci'
+            or parser.title['type'] == 'build'
+            or parser.title['type'] == 'docs'
+            or parser.title['type'] == 'test'
+            or parser.title['type'] == 'style'
+            or parser.title['type'] == 'perf'
+        ):
+            changes['changed'].extend(row)
+
+        if current_commit == commit:
+            break
+    return changes
 
 
 def generate_changelog(repo: 'Repository') -> None:
@@ -47,49 +96,9 @@ def generate_changelog(repo: 'Repository') -> None:
         )
 
         sections = ['commit', 'type', 'description']
-        scopes: Dict[str, List[str]] = {
-            'added': [],
-            'changed': [],
-            'deprecated': [],
-            'removed': [],
-            'fixed': [],
-            'security': [],
-        }
+        changes = get_release_changes(current_commit, repo)
 
-        parser = CommitMessageParser()
-        for commit in repo.walk(repo.head.target):
-            # check if commit is tagged and stop if it is
-            parser.parse(commit.message.rstrip())
-            scope = parser.title['scope']
-            row = [
-                commit.hex,
-                parser.title['type'],
-                parser.title['description']
-            ]
-
-            if scope is not None:
-                if scope not in scopes:
-                    scopes[scope] = []
-                scopes[scope].extend(row)
-            elif parser.title['type'] == 'feat':
-                scopes['added'].extend(row)
-            elif parser.title['type'] == 'fix':
-                scopes['fixed'].extend(row)
-            elif (
-                parser.title['type'] == 'refactor'
-                or parser.title['type'] == 'ci'
-                or parser.title['type'] == 'build'
-                or parser.title['type'] == 'docs'
-                or parser.title['type'] == 'test'
-                or parser.title['type'] == 'style'
-                or parser.title['type'] == 'perf'
-            ):
-                scopes['changed'].extend(row)
-
-        if current_commit == commit:
-            break
-
-        for k, v in scopes.items():
+        for k, v in changes.items():
             if v != []:
                 md.new_header(level=3, title=k)
                 md.new_table(
