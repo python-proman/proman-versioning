0caf123686765cba8121399c66835ef91590442b
Jesse P. Johnson <jpj6652@gmail.com>
2022-05-03 20:00:02 -0400

    ci: clean up troubleshooting

--- src/versioning/controller.py
+++ src/versioning/controller.py
@@ -13,7 +13,6 @@ from typing import TYPE_CHECKING, Any, Dict
 # from transitions import Machine
 from versioning.exception import PromanVersioningException
 from versioning.grammars.conventional_commits import CommitMessageParser
-from versioning.interactive import prepare_commit
 from versioning.version import Version
 
 if 'mdutils' not in sys.modules:
