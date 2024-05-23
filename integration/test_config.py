bb51a5aef50d108c1d4db5f8b840610fe719cfca
Jesse P. Johnson <jpj6652@gmail.com>
2024-05-23 13:05:07 -0500

    fix: rework default templates

--- integration/test_config.py
+++ integration/test_config.py
@@ -6,49 +6,108 @@
 
 import os
 
+import lark
+
 from versioning import Version, get_release_controller
 from versioning.config import GRAMMAR_PATH, Config
 
+COMMON_PATH = os.path.join(
+    os.path.dirname(lark.__file__), 'grammars', 'common.lark'
+)
+
+VERSION_PATH = os.path.abspath(
+    os.path.join(os.path.relpath(os.path.dirname(__file__)), '.version')
+)
+
 VERSIONING_PATH = os.path.abspath(
-    os.path.join(
-        os.path.relpath(os.path.dirname(__file__)),
-        '.versioning',
-    )
+    os.path.join(os.path.relpath(os.path.dirname(__file__)), '.versioning')
 )
 
 PYPROJECT_PATH = os.path.abspath(
-    os.path.join(
-        os.path.relpath(os.path.dirname(__file__)),
-        'pyproject.toml',
-    )
+    os.path.join(os.path.relpath(os.path.dirname(__file__)), 'pyproject.toml')
 )
 
 
 def test_proman_source_tree(fs):
     """Test source tree loading."""
     version = Version('1.2.3')
+    fs.add_real_file(VERSION_PATH)
     fs.add_real_file(VERSIONING_PATH)
     fs.add_real_file(PYPROJECT_PATH)
-    config = Config(filepaths=[VERSIONING_PATH, PYPROJECT_PATH])
+    config = Config(filepaths=[VERSION_PATH, VERSIONING_PATH, PYPROJECT_PATH])
     assert config.version.enable_devreleases is True
     assert config.version.enable_prereleases is True
     assert config.version.enable_postreleases is True
     assert config.version == version
-    assert Version(config.lookup('proman.version')) == version
+    assert Version(config.lookup('version')) == version
     assert Version(config.lookup('tool.proman.version')) == version
     assert Version(config.lookup('tool.poetry.version')) == version
 
 
-def test_proman_release_controller(fs):
+def test_version_release_controller(fs):
     """Test source tree loading."""
-    import lark
+    fs.add_real_file(COMMON_PATH)
+    fs.add_real_file(GRAMMAR_PATH)
+    fs.add_real_file(VERSION_PATH, False)
 
-    COMMON_PATH = os.path.join(
-        os.path.dirname(lark.__file__), 'grammars', 'common.lark'
+    repo_dir = os.path.abspath(
+        os.path.join(os.path.relpath(os.path.dirname(__file__)), '.gittest')
     )
+
+    controller = get_release_controller(
+        repo_dir=repo_dir, config_files=[VERSION_PATH]
+    )
+
+    version = controller.update_version(
+        commit=False,
+        tag=False,
+        tag_name='version',
+        message='test: this is a test',
+        sign=False,
+        dry_run=False,
+    )
+
+    config = Config(filepaths=[VERSION_PATH])
+
+    # TODO: need to compare version with commit
+    assert version == Version('1.2.3.post0')
+    assert Version(config.lookup('version')) == version
+
+
+def test_versioning_release_controller(fs):
+    """Test source tree loading."""
     fs.add_real_file(COMMON_PATH)
     fs.add_real_file(GRAMMAR_PATH)
     fs.add_real_file(VERSIONING_PATH, False)
+
+    repo_dir = os.path.abspath(
+        os.path.join(os.path.relpath(os.path.dirname(__file__)), '.gittest')
+    )
+
+    controller = get_release_controller(
+        repo_dir=repo_dir, config_files=[VERSIONING_PATH]
+    )
+
+    version = controller.update_version(
+        commit=False,
+        tag=False,
+        tag_name='version',
+        message='test: this is a test',
+        sign=False,
+        dry_run=False,
+    )
+
+    config = Config(filepaths=[VERSIONING_PATH])
+
+    # TODO: need to compare version with commit
+    assert version == Version('1.2.3.post0')
+    assert Version(config.lookup('version')) == version
+
+
+def test_pyproject_release_controller(fs):
+    """Test source tree loading."""
+    fs.add_real_file(COMMON_PATH)
+    fs.add_real_file(GRAMMAR_PATH)
     fs.add_real_file(PYPROJECT_PATH, False)
 
     repo_dir = os.path.abspath(
@@ -56,23 +115,22 @@ def test_proman_release_controller(fs):
     )
 
     controller = get_release_controller(
-        repo_dir=repo_dir,
-        config_files=[VERSIONING_PATH, PYPROJECT_PATH],
+        repo_dir=repo_dir, config_files=[PYPROJECT_PATH]
     )
+    print(controller.config.filepaths)
 
     version = controller.update_version(
         commit=False,
         tag=False,
         tag_name='version',
-        message='this is a test',
+        message='test: this is a test',
         sign=False,
         dry_run=False,
     )
 
-    config = Config(filepaths=[VERSIONING_PATH, PYPROJECT_PATH])
+    config = Config(filepaths=[PYPROJECT_PATH])
 
     # TODO: need to compare version with commit
     assert version == Version('1.2.3.post0')
-    assert Version(config.lookup('proman.version')) == version
     assert Version(config.lookup('tool.proman.version')) == version
     assert Version(config.lookup('tool.poetry.version')) == version
